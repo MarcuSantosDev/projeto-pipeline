b1ff2bb7a6b1df0f337b0d3946c15961007257d4
MarcuSantosDev <marcus.xtreme03@gmail.com>
2025-10-27 21:06:27 -0300

    "test: adiciona testes para página inicial"

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTests(TestCase):
+    def test_home_page_status_code(self):
+        """
+        Testa se a página inicial está acessível (HTTP 200).
+        """
+        response = self.client.get(reverse('home'))  
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_content(self):
+        """
+        Testa se o conteúdo da página inicial contém a mensagem da pipeline.
+        """
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, "🚀 Pipeline Django Funcionando!")
